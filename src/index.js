6c142c52d4ef344875fae9218775234c6716e1cf
Milan <milanm1120@gmail.com>
2022-02-08 23:23:00 -0500

    commit tree 686717239079737f7d3a59a1db7d18e117df262d

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ClickityClick from './components/ClickityClick';
 
 import DigitalClicker from './components/DigitalClicker';
 import YouTubeDebugger from './components/YouTubeDebugger';
 
 ReactDOM.render(
   <div>
+    <ClickityClick />
     <DigitalClicker />
     <YouTubeDebugger />
   </div>,
